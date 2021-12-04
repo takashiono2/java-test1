0ab9643fd62b90111dfcc09ed91b734f15b08f16
takashiono2 <takashiono710@gmail.com>
2021-12-04 20:01:05 +0900

    07まで

---
+++ MyApp.java
@@ -0,0 +1,10 @@
+public class MyApp{
+  public static void main(String[] args) {
+    // double d = 5234.231;
+    // int i = (int)d;
+    // System.out.println(i);
+    int i = 10;
+    double d = (double)i/4;
+    System.out.println(d);
+  }
+}
